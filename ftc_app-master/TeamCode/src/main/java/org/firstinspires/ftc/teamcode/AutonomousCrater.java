036308059818605105805b97688a9584978452c8
Alex18mai <alex18mai@gmail.com>
2019-01-21 18:17:11 +0200

    Clasele de autonomie

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousCrater.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode;
+
+public abstract class AutonomousCrater extends AutonomousMode {
+}
---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousMode.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public abstract class AutonomousMode extends LinearOpMode {
+    protected abstract void runOp();
+    protected abstract void endOp();
+}
