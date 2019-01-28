96ae75ec06ff5d60b19024e10d6c32ffea9b7e12
Alex18mai <alex18mai@gmail.com>
2019-01-28 16:11:31 +0200

    AUTONOMIE CRATER SIMPLA

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousTest.java
@@ -7,136 +7,86 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 import static java.lang.Math.abs;
-import static java.lang.Math.min;
-import static java.lang.Math.max;
 
 @Autonomous(name = "Autonomous_test", group = "Autonomous")
 
-public class AutonomousTest extends LinearOpMode
-{
+public class AutonomousTest extends LinearOpMode {
 
     //motoare
-    private DcMotor mers_left = null;
-    private DcMotor mers_right = null;
-    private DcMotor glisare = null;
-    private DcMotor ridicare_cutie = null;
-    private DcMotor ridicare_perii = null;
-    private DcMotor rotire_perii = null;
+    protected DcMotor mers_left = null;
+    protected DcMotor mers_right = null;
+    protected DcMotor glisare = null;
+    protected DcMotor ridicare_cutie = null;
+    protected DcMotor ridicare_perii = null;
+    protected DcMotor rotire_perii = null;
 
     //senzori
-    //private GyroSensor gyro = null;
-    private ModernRoboticsI2cGyro gyro = null;
-    private ModernRoboticsI2cRangeSensor range_right = null;
-    private ModernRoboticsI2cRangeSensor range_left = null;
-    private ModernRoboticsI2cColorSensor color = null;
+    protected ModernRoboticsI2cGyro gyro = null;
+    protected ModernRoboticsI2cRangeSensor range_right = null;
+    protected ModernRoboticsI2cRangeSensor range_left = null;
+    protected ModernRoboticsI2cColorSensor color = null;
 
-    int cnst = 1000;
+    final int const_sleep = 1000;
+    final int const_encoder = 67;
 
     @Override
-    public void runOpMode()
-    {
+    public void runOpMode() {
         initialise();
 
         waitForStart();
 
-        //crater simplu :
+        boolean test_rotit = false;
+        boolean test_mers_encoder = false;
 
-        //ma misc in fata pana ajung pe linia obiectelor
-        //ma intorc ca sa fiu paralel cu obiectele
-        //dau in spate pana la ultimul obiect
-        //verific
-        //merg pana la al doilea
-        //verific
-        //merg pana la al treilea
-        //verific
-        //cand gasesc cul == galben fac 90 grade , merg in fata si ma opresc pe crater
+        boolean test_senz = false;
+        boolean test_mers_culoare = true;
 
-        //--------------------------------------------------------------------------
-
-        //crater cu optimizare :
-
-        //ma misc in fata pana ajung pe linia obiectelor
-        //ma intorc ca sa fiu paralel cu obiectele
-        //dau in spate pana la ultimul obiect
-        //verific
-        //merg pana la al doilea
-        //verific
-        //merg pana la al treilea
-        //verific
-        //cand gasesc cul == galben fac 90 , un fata spate , -90
-        //ma duc pana la perete si ma pun paralel si ma pun pe crater
-
-        //---------------------------------------------------------------------------
-
-        boolean test_gyro = true;
-        boolean test_PID_angle = false;
-        boolean test_PID_walk = false;
-        boolean test_cul = false;
-        boolean test_range = false;
-        boolean test_simple_walk = false;
-
-        if (test_gyro){
-            while (opModeIsActive())
-            {
-                telemetry.addData ("unghi : " , gyro.getHeading());
+        if (test_senz) {
+            while (opModeIsActive()) {
+                telemetry.addData("distanta dreapta : ", range_right.rawUltrasonic());
+                telemetry.addData("distanta stanga : ", range_left.rawUltrasonic());
+                telemetry.addData("culoare : ", color.readUnsignedByte(ModernRoboticsI2cColorSensor.Register.COLOR_NUMBER));
+                telemetry.addData("calibrat : ", gyro.isCalibrating());
+                telemetry.addData("unghi : ", gyro.getHeading());
                 telemetry.update();
             }
         }
 
-        if (test_PID_angle){
-            PID_angle(90.0);
-
-            sleep(3 * cnst);
-
-            PID_angle(-90.0);
+        if (test_rotit) {
+            rotit(90);
+            sleep(3 * const_sleep);
+            rotit(-90);
+            sleep(3 * const_sleep);
+            rotit(180);
+            sleep(3 * const_sleep);
+            rotit(-180);
+            sleep(3 * const_sleep);
+            rotit(20);
+            sleep(3 * const_sleep);
+            rotit(-20);
         }
 
-        if (test_PID_walk){
-            PID_walk(5);
+        if (test_mers_encoder) {
+            mers_encoder(50 * const_encoder);
+            sleep(3 * const_sleep);
 
-            sleep(3 * cnst);
+            mers_encoder(-100 * const_encoder);
+            sleep(3 * const_sleep);
 
-            PID_walk(-5);
+            mers_encoder(30 * const_encoder);
+            sleep(3 * const_sleep);
+
+            mers_encoder(-70 * const_encoder);
         }
 
-        if (test_simple_walk){
-            simple_walk(5);
-
-            sleep(3 * cnst);
-
-            simple_walk(-5);
+        if (test_mers_culoare) {
+            mers_culoare();
         }
-
-        if (test_cul){
-            while (opModeIsActive())
-            {
-                telemetry.addData ("culoare : " , color.readUnsignedByte(ModernRoboticsI2cColorSensor.Register.COLOR_NUMBER));
-                telemetry.update();
-            }
-        }
-
-        if (test_range){
-            while (opModeIsActive())
-            {
-                telemetry.addData ("left ultrasonic : " , range_left.rawUltrasonic());
-                telemetry.addData ("left optical : " , range_left.rawOptical());
-                telemetry.addData ("left cm : " , range_left.getDistance(DistanceUnit.CM));
-
-                telemetry.addData ("right ultrasonic : " , range_right.rawUltrasonic());
-                telemetry.addData ("right optical : " , range_right.rawOptical());
-                telemetry.addData ("right cm : " , range_right.getDistance(DistanceUnit.CM));
-
-                telemetry.update();
-            }
-        }
-
     }
 
-    private void initialise()
-    {
+    protected void initialise() {
         //mapare
         mers_left = hardwareMap.dcMotor.get("mers_left");
         mers_right = hardwareMap.dcMotor.get("mers_right");
@@ -147,10 +97,10 @@ public class AutonomousTest extends LinearOpMode
 
         //senzori
         //gyro = hardwareMap.gyroSensor.get("gyro");
-        gyro = hardwareMap.get(ModernRoboticsI2cGyro.class ,"gyro");
-        range_left = hardwareMap.get(ModernRoboticsI2cRangeSensor.class ,"range_left");
-        range_right = hardwareMap.get(ModernRoboticsI2cRangeSensor.class ,"range_right");
-        color = hardwareMap.get(ModernRoboticsI2cColorSensor.class , "color");
+        gyro = hardwareMap.get(ModernRoboticsI2cGyro.class, "gyro");
+        range_left = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "range_left");
+        range_right = hardwareMap.get(ModernRoboticsI2cRangeSensor.class, "range_right");
+        color = hardwareMap.get(ModernRoboticsI2cColorSensor.class, "color");
 
         //setare puteri la 0
         mers_left.setPower(0);
@@ -168,130 +118,159 @@ public class AutonomousTest extends LinearOpMode
         ridicare_perii.setDirection(DcMotorSimple.Direction.FORWARD);
         rotire_perii.setDirection(DcMotorSimple.Direction.FORWARD);
 
+        mers_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mers_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
         //calibreat gyro
         gyro.calibrate();
+        while (gyro.isCalibrating()) {
+            idle();
+        }
 
         //senzor de culoare
         color.enableLed(true); //daca ma uit la un : obiect - true ; lumina - false
-
     }
 
-     private void PID_angle (double angle){
-
-         //modific unghil momentat cu angle
-         //cred ca am pb cand sare la minus sau cacaturi dinastea
-
-         double start = gyro.getHeading();
-
-         double pGain = 0.3 ;
-         double iGain = 0.2;
-         double dGain = 0.1;
-
-         double error = angle;
-         double sum = angle;
-
-         while (error > 1 || error < 1){
-             double now = gyro.getHeading();
-             if (now > 180){
-                 now -= 360;
-                 telemetry.addLine("sa imi bag pula");
-                 break;
-             }
-             double last = error;
-             error = angle - abs(now - start);
-             sum += error;
-             double derivata = error - last;
-
-             double speed = (error * pGain) + (sum * iGain) + (derivata * dGain);
-
-             telemetry.addData("speed : ", speed );
-             telemetry.addData("error : ", error );
-             telemetry.addData("unghi : ", now );
-             telemetry.update();
-
-             double absolut = abs(speed);
-             absolut = min(absolut , 0.5);
-             if (speed < 0){
-                 speed = -absolut;
-             }
-             else{
-                 speed = absolut;
-             }
-
-             mers_left.setPower(speed);
-             mers_right.setPower(-speed);
-         }
-
-         mers_left.setPower(0);
-         mers_right.setPower(0);
-     }
-
-     private boolean culoare(){
-        int cul = color.readUnsignedByte(ModernRoboticsI2cColorSensor.Register.COLOR_NUMBER);
-        return (cul == 8);
-     }
-
-    private void simple_walk (double walk){
-
-        //merge walk cm (daca e cu minus in spate , altfel in fata)
-
-        double start = range_right.getDistance(DistanceUnit.CM);
-        double now = range_right.getDistance(DistanceUnit.CM);
-
-        int dir = 1;
-        if (walk < 0){
-            dir = -1;
+    private void rotit(double angle) {
+        if (abs(angle) <= 1) {
+            return;
         }
+        double end = gyro.getHeading() + angle;
+        while (end < 0) {
+            end += 360;
+        }
+        double speed = 0.6;
+        if (angle < 0) {
+            speed = -speed;
+        }
+        mers_left.setPower(-speed);
+        mers_right.setPower(speed);
+        while (abs(gyro.getHeading() - end) > 5 && opModeIsActive()) {
+            idle();
+        }
+        rotit_delicat(end, speed);
+    }
 
-        mers_left.setPower(0.5 * dir);
-        mers_right.setPower(0.5 * dir);
-        while (abs(start - now) < abs(walk)){
+    protected void rotit_delicat(double end, double speed) {
+        if (speed > 0) {
+            speed = 0.2;
+        } else {
+            speed = -0.2;
+        }
+        mers_left.setPower(-speed);
+        mers_right.setPower(speed);
+        while (abs(gyro.getHeading() - end) > 1 && opModeIsActive()) {
+            idle();
         }
         mers_left.setPower(0);
         mers_right.setPower(0);
     }
 
-     private void PID_walk (double walk){
+    /*protected void mers_ultrasonic (double pasi){
+        //cu pasi unde mai multe -> pasi poz se duce in spate , pasi neg in fata
+        double end = range_left.rawUltrasonic() + pasi;
+        double speed = 0.3;
+        if (pasi > 0){
+            speed = -speed;
+        }
+        mers_left.setPower(speed);
+        mers_right.setPower(speed);
+        while (abs(range_left.rawUltrasonic() - end) > 5 && opModeIsActive()){
+        }
+        //mers_left.setPower(0);
+        //mers_right.setPower(0);
+        mers_delicat_ultrasonic(end , speed);
+    }
 
-         //merge walk cm (daca e cu minus in spate , altfel in fata)
+    protected void mers_delicat_ultrasonic (double end , double speed){
+        if (speed > 0){
+            speed = 0.15;
+        }
+        else{
+            speed = -0.15;
+        }
+        mers_left.setPower(speed);
+        mers_right.setPower(speed);
+        while (abs(range_left.rawUltrasonic() - end) > 1 && opModeIsActive()){
+            telemetry.addData("dist delicat : " , range_left.rawUltrasonic());
+            telemetry.update();
+        }
+        mers_left.setPower(0);
+        mers_right.setPower(0);
+    }*/
 
-         double start = range_right.getDistance(DistanceUnit.CM);
+    protected void mers_encoder(int pasi) {
+        if (abs(pasi) > 0) {
+            mers_left.setTargetPosition(mers_left.getCurrentPosition() + pasi * const_encoder);
+            mers_right.setTargetPosition(mers_right.getCurrentPosition() + pasi * const_encoder);
+        }
+        mers_left.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        mers_left.setPower(0.7);
+        mers_right.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        mers_right.setPower(0.7);
+        while (mers_left.isBusy() || mers_right.isBusy()) {
+            idle();
+        }
+        mers_left.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mers_left.setPower(0);
+        mers_right.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        mers_right.setPower(0);
+    }
 
-         double pGain = 0.3 ;
-         double iGain = 0.2;
-         double dGain = 0.1;
+    protected void mers_cul() {
+        double speed = 0.2;
+        mers_left.setPower(speed);
+        mers_right.setPower(speed);
+        while (color.readUnsignedByte(ModernRoboticsI2cColorSensor.Register.COLOR_NUMBER) == 0 && opModeIsActive()) {
+            idle();
+        }
+        mers_left.setPower(0);
+        mers_right.setPower(0);
+    }
 
-         double error = walk;
-         double sum = walk;
+    protected boolean culoare() {
+        int cul = color.readUnsignedByte(ModernRoboticsI2cColorSensor.Register.COLOR_NUMBER);
+        return (cul == 8 || cul == 9 || cul == 10);
+    }
 
-         while (error > 0.1 || error < 0.1){
-             double now = range_right.getDistance(DistanceUnit.CM);
-             double last = error;
-             error = walk - (now-start);
-             sum += error;
-             double derivata = error - last;
+    private boolean etapa(double angle) {
+        rotit(angle);
+        int sl = mers_left.getCurrentPosition();
+        int sr = mers_right.getCurrentPosition();
+        mers_encoder(25);
+        mers_cul();
+        boolean ok = false;
+        if (culoare()) {
+            mers_encoder(5);
+            mers_encoder(-5);
+            ok = true;
+        }
+        mers_left.setTargetPosition(sl);
+        mers_right.setTargetPosition(sr);
+        mers_encoder(0);
+        rotit(-angle);
+        return ok;
+    }
 
-             double speed = (error * pGain) + (sum * iGain) + (derivata * dGain);
+    protected void mers_culoare() {
+        if (etapa(0)) {
+            mers_left.setPower(0);
+            mers_right.setPower(0);
+            return;
+        }
+        sleep(300);
+        if (etapa(-28)) {
+            mers_left.setPower(0);
+            mers_right.setPower(0);
+            return;
+        }
+        sleep(300);
+        if (etapa(28)) {
+            mers_left.setPower(0);
+            mers_right.setPower(0);
+            return;
+        }
+    }
 
-             telemetry.addData("speed : ", speed );
-             telemetry.addData("error : ", error );
-             telemetry.addData("dist : ", now );
-             telemetry.update();
-
-             double absolut = abs(speed);
-             absolut = min(absolut , 0.5);
-             if (speed < 0){
-                 speed = -absolut;
-             }
-             else{
-                 speed = absolut;
-             }
-
-             mers_left.setPower(speed);
-             mers_right.setPower(speed);
-         }
-
-         mers_left.setPower(0);
-         mers_right.setPower(0);
-     }
 }
+
