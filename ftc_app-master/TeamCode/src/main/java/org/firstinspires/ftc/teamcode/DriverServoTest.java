8943f7d5557590917b59a71073564a39a5043485
DESKTOP-8480LNG\robos <daniel27peter@gmail.com>
2019-03-01 20:44:05 +0200

    functie

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverServoTest.java
@@ -0,0 +1,41 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.ServoController;
+
+public class DriverServoTest extends LinearOpMode {
+
+    private Servo ServoL = null;
+    private Servo ServoR = null;
+
+    @Override
+    public void runOpMode() {
+
+        ServoL = hardwareMap.servo.get("ServoL");
+        ServoR = hardwareMap.servo.get("ServoR");
+
+        ServoL.setDirection(Servo.Direction.FORWARD);
+        ServoR.setDirection(Servo.Direction.REVERSE);
+
+        telemetry.addData("Status :", "wait for start");
+        telemetry.update();
+
+        waitForStart();
+
+        while(opModeIsActive()){
+            if(gamepad1.right_bumper){
+                ServoL.setPosition(Servo.MAX_POSITION);
+                ServoR.setPosition(Servo.MAX_POSITION);
+            }
+            else if(gamepad1.left_bumper){
+                ServoL.setPosition(Servo.MIN_POSITION);
+                ServoR.setPosition(Servo.MIN_POSITION);
+            }
+        }
+
+
+    }
+
+
+}
